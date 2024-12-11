637cc71b77565eb48908a612ed3f43fc93fd386d
Rajwinder <84006059+rajwindersxxx@users.noreply.github.com>
2024-12-11 14:28:21 +0000

    make componet proejct

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,119 +1,41 @@
 export default function Projects() {
   return (
-    <section id="projects" className="Projects_section container">
-      <div className="project_block">
+    <section id="projects" className="w-full text-teal-200">
+      <div className=" container border m-auto border-secondaryColor p-8">
         <div>
-          <h2>Explore some of my projects</h2>
+          <h2 className="text-4xl text-center mb-8 ">
+            Explore some of my projects
+          </h2>
         </div>
-        <div className="grid grid_col-3">
-          <div className="project">
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8">
+          <div className="flex flex-col gap-4">
             <div className="image">
-              <img className="lazy" src="./src/images/notenestProject.png" alt="erms project" />
+              <img
+                className="lazy"
+                src="./img/notenestProject.png"
+                alt="erms project"
+              />
             </div>
-            <h3>NoteNest (make nots)</h3>
-            <p>
-              Created entirely by me, this book review app allows users to share their thoughts and summaries using EJS
-              templates and Passport for secure OAuth authentication.
+            <h3 className="text-3xl text-teal-300">NoteNest (make nots)</h3>
+            <p className="text-teal-400">
+              Created entirely by me, this book review app allows users to share
+              their thoughts and summaries using EJS templates and Passport for
+              secure OAuth authentication.
             </p>
             <div className="Project_links">
-              <a href="https://notenest.tiven.xyz/" target="_blank" rel="noopener">
+              <a
+                href="https://notenest.tiven.xyz/"
+                target="_blank"
+                rel="noopener"
+              >
                 [View Project]
               </a>
               |
-              <a href="https://github.com/rajwindersxxx/notebook-Site.git" target="_blank" rel="noopener">
-                [Git Hub]
-              </a>
-            </div>
-          </div>
-          <div className="project">
-            <div className="image">
-              <img className="lazy" src="./src/images/maptyProject.png" alt="erms project" />
-            </div>
-            <h3>Mapty Workout Tracker</h3>
-            <p>
-              This workout tracker, developed as part of a course challenge, uses HTML, SCSS, and JavaScript. I added
-              features like local storage, weather integration, error handling, api calls and editing options to enhance
-              functionality.
-            </p>
-            <div className="Project_links">
-              <a href="https://mapty.tiven.xyz/" target="_blank" rel="noopener">
-                [View Project]
-              </a>
-              |
-              <a href="https://github.com/rajwindersxxx/maptyChallange.git" target="_blank" rel="noopener">
-                [Git Hub]
-              </a>
-            </div>
-          </div>
-          <div className="project">
-            <div className="image">
-              <img className="lazy" src="./src/images/ermsProject.png" alt="erms project" />
-            </div>
-            <h3>Employee management</h3>
-            <p>
-              Fully developed by me, this PHP-based application manages employee records. It incorporates HTML, CSS,
-              JavaScript, and Bootstrap for a responsive design.
-            </p>
-            <div className="Project_links">
-              <a href="http://ermsproject.atwebpages.com/" target="_blank" rel="noopener">
-                [View Project]
-              </a>
-              |
-              <a href="https://github.com/rajwindersxxx/employeemanagement.git" target="_blank" rel="noopener">
-                [Git Hub]
-              </a>
-            </div>
-          </div>
-          <div className="project">
-            <div className="image">
-              <img className="lazy" src="./src/images/omifoodProject.png" alt="erms project" />
-            </div>
-            <h3>Product Landing Page</h3>
-            <p>
-              A clean, stylish landing page created with HTML, CSS, and a bit of JavaScript, showcasing product
-              promotion.
-            </p>
-            <div className="Project_links">
-              <a href="https://ommifoodapp.netlify.app/" target="_blank" rel="noopener">
-                [View Project]
-              </a>
-              |
-              <a href="https://github.com/rajwindersxxx/Simon-Game-Challenge.git" target="_blank" rel="noopener">
-                [Git Hub]
-              </a>
-            </div>
-          </div>
-          <div className="project">
-            <div className="image">
-              <img className="lazy" src="./src/images/gameProject.png" alt="erms project" />
-            </div>
-            <h3>Memory Challenge Game</h3>
-            <p>
-              A course challenge where players replicate growing patterns. Built using HTML, CSS, and JavaScript, with
-              materials provided by the instructor.
-            </p>
-            <div className="Project_links">
-              <a href="https://siamongame.tiven.xyz" target="_blank" rel="noopener">
-                [View Project]
-              </a>
-              |
-              <a href="https://github.com/rajwindersxxx/Simon-Game-Challenge" target="_blank" rel="noopener">
-                [Git Hub]
-              </a>
-            </div>
-          </div>
-          <div className="project">
-            <div className="image">
-              <img className="lazy" src="./src/images/KeeperProject.png" alt="erms project" />
-            </div>
-            <h3>Keeper App</h3>
-            <p>Developed using React, this is the note-taking app allows users to create and save notes.</p>
-            <div className="Project_links">
-              <a href="https://keeperapp.tiven.xyz/" target="_blank" rel="noopener">
-                [View Project]
-              </a>
-              |
-              <a href="https://github.com/rajwindersxxx/keeper-app" target="_blank" rel="noopener">
+              <a
+                href="https://github.com/rajwindersxxx/notebook-Site.git"
+                target="_blank"
+                rel="noopener"
+              >
                 [Git Hub]
               </a>
             </div>
